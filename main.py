369301f59db3f39e9cb2d0d3b9ed8367ddb6aa58
MAC <mac@James-Mac.local>
2025-01-24 12:29:32 +0100

    changes to main.py"

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("Hello World")
 print("Despicable AI")
 print("Hello Mama")
+
+
+
+import pandas as pd
+file_path = ""
+try:
+    df = pd.read_csv(file_path)
+except:
+    print("file_path does not exist")
